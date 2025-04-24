16f29a25183cf92943f0b9b805f9a47a2595bfb8
Ryan Tan <ryantan.mail@gmail.com>
2025-04-23 21:50:49 -0700

    Update: coin flip words with more personality

--- scripts/coin.js
+++ scripts/coin.js
@@ -1,13 +1,26 @@
-
 const coinFlipButton = document.querySelector(".coin-flip-button");
-coinFlipButton.addEventListener("click", flipCoinWord);
+coinFlipButton.addEventListener("click", flipCoin);
 
-function flipCoinWord() {
+function flipCoin() {
+	const coinFlipMode = document.querySelector("select[name=\"coin-mode\"]").value;
+	switch (coinFlipMode) {
+		case "Minimal": 
+			flipCoinMinimal();
+			break;
+		case "Basic":
+			break;
+		case "Deluxe":
+			break;
+	}
+}
+
+function flipCoinMinimal() {
 	const coinCanvas = document.querySelector(".coin-canvas");
-	if (Math.random() > .5) {
-		coinCanvas.innerHTML = "Heads";
+	const coinResult = (Math.random() > .5) ? "Heads" : "Tails" ;
+	if (coinResult  == coinCanvas.innerHTML.slice(0, 5))  {
+		coinCanvas.innerHTML += " again";
 	} else {
-		coinCanvas.innerHTML = "Tails"
+		coinCanvas.innerHTML = coinResult;
 	}
 }
 
